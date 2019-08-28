3c4e62e087c1ec82330a12221f4948c15e324776
Yogesh Pol <38473308+yogi14pol@users.noreply.github.com>
2019-08-28 19:33:47 +0530

    Viewflipper

---
+++ app/src/main/java/com/clicks/yogi/viewflipper/MainActivity.java
@@ -0,0 +1,25 @@
+package com.clicks.yogi.viewflipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper viewFlipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewflip);
+        viewFlipper.startFlipping();
+        viewFlipper.setFlipInterval(2000);
+    }
+}
